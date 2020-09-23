b4ed2afc86a7b1e0933754d8ac032ea1c1419eb5
sungkuk5420 <sungkuk5420@gmail.com>
2020-09-24 01:58:10 +0900

    메인 레이아웃 다듬기

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,33 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header bordered class="bg-primary text-white" height-hint="98">
+    <q-header
+      bordered
+      class="bg-primary text-white"
+      height-hint="98"
+      v-show="false"
+    >
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left"/>
-
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>Title
+            <img
+              src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
+            /> </q-avatar
+          >Title
         </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="right = !right"/>
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One"/>
-        <q-route-tab to="/page2" label="Page Two"/>
-        <q-route-tab to="/page3" label="Page Three"/>
-      </q-tabs>
     </q-header>
-
-    <q-drawer show-if-above v-model="left" side="left" behavior="desktop" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
-    <q-drawer show-if-above v-model="right" side="right" behavior="desktop" elevated>
-      <!-- drawer content -->
-    </q-drawer>
-
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
 
-    <q-footer reveal class="bg-grey-8 text-white">
+    <q-footer reveal class="bg-grey-8 text-white" v-show="false">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>Title
+            <img
+              src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
+            /> </q-avatar
+          >Title
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
@@ -46,11 +36,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      left: false,
-      right: false
-    };
-  }
+  data() {},
 };
 </script>
