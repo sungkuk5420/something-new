845c05e3d593e90b24bc0f06db158e5308e6f8fc
juyeol <ljykimc159@gmail.com>
2020-09-26 16:51:23 +0900

    footer 추가 및 링크 연결

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -20,22 +20,27 @@
       <router-view />
     </q-page-container>
 
-    <q-footer reveal class="bg-grey-8 text-white" v-show="false">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img
-              src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
-            /> </q-avatar
-          >Title
-        </q-toolbar-title>
-      </q-toolbar>
+    <q-footer reveal class="bg-grey-8 text-white" v-show="true">
+      <van-tabbar v-model="active">
+        <van-tabbar-item to="/main" icon="home-o">main</van-tabbar-item>
+        <van-tabbar-item to="/userList" icon="search">userList</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">Tab</van-tabbar-item>
+        <van-tabbar-item to="/setting" icon="setting-o"
+          >setting</van-tabbar-item
+        >
+      </van-tabbar>
     </q-footer>
   </q-layout>
 </template>
 
 <script>
+import { Tabbar, TabbarItem } from "vant";
+
 export default {
-  data() {},
+  data() {
+    return {
+      active: 0,
+    };
+  },
 };
 </script>
