29517723cf2e5840be3748a31806519a5678d1bc
이승태 <fo_ma@naver.com>
2020-10-02 02:09:21 +0900

    [페이지 시안] 프로필관리 - (세트) 음주, 흡연 모달 모달을 어케하는지 몰라서.. 일단 페이지로 작업했습니다

---
+++ src/pages/Drink.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page class="drink-page">
+    <van-nav-bar
+      title="음주"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    >
+    </van-nav-bar>
+
+    <div class="list">
+      <ul>
+        <li>술을 못해요</li>
+        <li>가끔 해요</li>
+        <li>적당히 마셔요</li>
+        <li>많이 마셔요</li>
+        <li>술고래에요</li>
+        <li>조금씩 자주 마셔요</li>
+      </ul>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.drink-page {
+  .van-icon-arrow-left::before {
+    color: #000;
+  }
+  .list {
+    ul {
+      li {
+        padding: 18px 24px;
+        border-bottom: 1px solid #f0f1f3;
+        font-family: Noto Sans KR;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 20px;
+        color: #121214;
+      }
+      li:last-child {
+        border-bottom: none;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/Smoke.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page class="smoke-page">
+    <van-nav-bar
+      title="흡연"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    >
+    </van-nav-bar>
+
+    <div class="list">
+      <ul>
+        <li>하지 않아요</li>
+        <li>가끔 해요</li>
+        <li>자주 해요</li>
+        <li>비흡연자 앞에서는 하지 않아요</li>
+        <li>상대방이 싫어하면 끊을 수 있어요</li>
+      </ul>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.smoke-page {
+  .van-icon-arrow-left::before {
+    color: #000;
+  }
+  .list {
+    ul {
+      li {
+        padding: 18px 24px;
+        border-bottom: 1px solid #f0f1f3;
+        font-family: Noto Sans KR;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 20px;
+        color: #121214;
+      }
+      li:last-child {
+        border-bottom: none;
+      }
+    }
+  }
+}
+</style>
