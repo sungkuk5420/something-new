eb5924c9a6e1adee0ab63d267a9a5292a949b805
kim_mindfree <kim_sungkuk@mindfree.co.jp>
2020-12-13 00:22:11 +0900

    메인하단 메뉴 스타일링.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,29 +15,25 @@
     <q-footer reveal class="bg-grey-8 text-white" v-show="loginUser !== null">
       <van-tabbar v-model="active">
         <van-tabbar-item to="/main">
-          <span>home</span>
           <template #icon="home">
             <!--img :src="home.active ? icon.active : icon.inactive" /-->
-            <img class="off" src="../assets/home.svg" />
             <img class="on" src="../assets/home_on.svg" />
+            <img class="off" src="../assets/home.svg" />
           </template>
         </van-tabbar-item>
         <van-tabbar-item to="/chatlist">
-          <span>chat</span>
           <template #icon="chat">
             <img class="off" src="../assets/chat.svg" />
             <img class="on" src="../assets/chat_on.svg" />
           </template>
         </van-tabbar-item>
         <van-tabbar-item to="/ParticipationList">
-          <span>card</span>
           <template #icon="card">
             <img class="off" src="../assets/card.svg" />
             <img class="on" src="../assets/card_on.svg" />
           </template>
         </van-tabbar-item>
         <van-tabbar-item to="/setting">
-          <span>menu</span>
           <template #icon="menu">
             <img class="off" src="../assets/menu.svg" />
             <img class="on" src="../assets/menu_on.svg" />
@@ -59,22 +55,18 @@ export default {
       init: false,
       active: 0,
       icon: {
-        name: "home",
         active: "../assets/home.svg",
         inactive: "../assets/home_on.svg",
       },
       icon: {
-        name: "chat",
         active: "../assets/chat.svg",
         inactive: "../assets/chat_on.svg",
       },
       icon: {
-        name: "card",
         active: "../assets/card.svg",
         inactive: "../assets/card_on.svg",
       },
       icon: {
-        name: "menu",
         active: "../assets/menu.svg",
         inactive: "../assets/menu_on.svg",
       },
@@ -98,10 +90,21 @@ export default {
 </script>
 <style lang="scss">
 .q-footer {
-  .on {
-    display: none;
-  }
-  .off {
+  .van-tabbar-item {
+    .on {
+      display: none;
+    }
+    .off {
+      display: block;
+    }
+    &--active {
+      .on {
+        display: block;
+      }
+      .off {
+        display: none;
+      }
+    }
   }
 }
 </style>
